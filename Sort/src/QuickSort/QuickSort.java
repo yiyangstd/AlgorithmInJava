112f6eec5b8d40aae817d9385dc7f8d8280680ea
yangyi <yangyistd@163.com>
2014-09-26 19:51:40 +0800

    The first commit

---
+++ Sort/src/QuickSort/QuickSort.java
@@ -0,0 +1,10 @@
+package QuickSort;
+
+/**
+ * Created by yangyi on 2014/9/26.
+ */
+public class QuickSort {
+    public static void main(String[] args){
+        System.out.println("Quick Sort");
+    }
+}
