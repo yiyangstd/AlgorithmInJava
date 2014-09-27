67379a3ed6ebc4391b35ba4942282e9c3b655fd8
yangyi <yangyistd@163.com>
2014-09-28 00:18:45 +0800

    QuickSort

--- Sort/src/QuickSort/QuickSort.java
+++ Sort/src/QuickSort/QuickSort.java
@@ -1,9 +1,28 @@
 package QuickSort;
+import SortModel.SortModel;
 
 /**
  * Created by yangyi on 2014/9/26.
  */
 public class QuickSort {
+    public static void sort(Comparable a[]){
+        quickSort(a, 0, a.length);
+    }
+
+    private static void quickSort(Comparable a[], int i, int j){
+        if(i < j){
+            Comparable temp = a[i];
+            while(i < j){
+                while(SortModel.less(temp, a[j])){
+                    j --;
+                }
+
+                while(SortModel.less(a[i], temp)){
+                    i ++;
+                }
+            }
+        }
+    }
     public static void main(String[] args){
         System.out.println("Quick Sort");
     }
